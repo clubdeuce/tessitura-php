4caf0cfba679a82f8dab6f5dccc91740e3304a8b
Daryl <daryl@clubdeuce.net>
2024-11-08 21:06:31 -0500

    Implement season resource

---
+++ src/Resources/Season.php
@@ -0,0 +1,75 @@
+<?php
+namespace Clubdeuce\Tessitura\Resources;
+
+use Clubdeuce\Tessitura\Base\Base;
+use DateTime;
+use DateTimeZone;
+
+class Season extends Base {
+
+    function createdDateTime(string $timezone = 'America/New_York') : ?DateTime {
+        try {
+            $timezone = new DateTimeZone($timezone);
+        } catch (\Exception $exception) {
+            trigger_error($exception->getMessage(), E_USER_WARNING);
+        }
+
+        if(isset($this->extra_args()['CreatedDateTime'])) {
+            try {
+                return new DateTime($this->extra_args()['CreatedDateTime'], $timezone);
+            } catch (\Exception $e) {
+                trigger_error($e->getMessage(), E_USER_WARNING);
+            }
+        }
+
+        return null;
+    }
+
+    function description(): string
+    {
+        return (string)$this->extra_args()['Description'];
+    }
+
+    function endDateTime($timezone = 'America/New_York') : ?DateTime
+    {
+        try {
+            $timezone = new DateTimeZone($timezone);
+        } catch (\Exception $exception) {
+            trigger_error($exception->getMessage(), E_USER_WARNING);
+        }
+
+        if(isset( $this->extra_args()['EndDateTime'])) {
+            try {
+                return new \DateTime( $this->extra_args()['EndDateTime'], $timezone );
+            } catch (\Exception $exception) {
+                trigger_error($exception->getMessage(), E_USER_WARNING);
+            }
+        }
+
+        return null;
+    }
+
+    function id(): int
+    {
+        return intval($this->extra_args()['Id']);
+    }
+
+    function startDateTime(string $timezone = 'America/New_York'): ?\DateTime
+    {
+        try {
+            $timezone = new DateTimeZone($timezone);
+        } catch (\Exception $exception) {
+            trigger_error($exception->getMessage(), E_USER_WARNING);
+        }
+
+        if (isset($this->extra_args()['StartDateTime'])) {
+            try {
+                return new \DateTime( $this->extra_args()['StartDateTime'], $timezone );
+            } catch (\Exception $e) {
+                trigger_error($e->getMessage(), E_USER_WARNING);
+            }
+        }
+
+        return null;
+    }
+}
---
+++ tests/unit/SeasonTest.php
@@ -0,0 +1,73 @@
+<?php
+
+use Clubdeuce\Tessitura\Resources\Season;
+
+#[\PHPUnit\Framework\Attributes\CoversClass(Season::class)]
+class SeasonTest extends \tessitura\tests\includes\testCase
+{
+
+    /**
+     * @var Season
+     */
+    protected Season $_sut;
+
+    public function setUp(): void
+    {
+        $data = json_decode(file_get_contents(dirname(__DIR__) . '/fixtures/season.json'), 'associative');
+        $this->_sut = new Season($data);
+    }
+
+    public function testId(): void
+    {
+        $this->assertIsInt($this->_sut->id());
+        $this->assertEquals(194, $this->_sut->id());
+    }
+
+    public function testDescription()
+    {
+        $this->assertIsString($this->_sut->description());
+        $this->assertEquals('2024/2025 Opera', $this->_sut->description());
+    }
+
+    public function testCreatedDateTime(): void
+    {
+        $created = $this->_sut->createdDateTime();
+
+        $this->assertInstanceOf(\DateTime::class, $created);
+        $this->assertEquals('2024-01-19', $created->format('Y-m-d'));
+    }
+
+    public function testStartDateTime()
+    {
+        $start = $this->_sut->startDateTime();
+
+        $this->assertInstanceOf(\DateTime::class, $start);
+        $this->assertEquals('2024-07-01', $start->format('Y-m-d'));
+    }
+
+    public function testEndDateTime()
+    {
+        $end = $this->_sut->endDateTime();
+
+        $this->assertInstanceOf(\DateTime::class, $end);
+        $this->assertEquals('2025-06-30', $end->format('Y-m-d'));
+    }
+
+    public function testCreatedDateTimeIsNull(): void
+    {
+        $season = new Season();
+        $this->assertNull($season->createdDateTime());
+    }
+
+    public function testStartDateTimeIsNull()
+    {
+        $season = new Season();
+        $this->assertNull($season->startDateTime());
+    }
+
+    public function testEndDateTimeIsNull()
+    {
+        $season = new Season();
+        $this->assertNull($season->endDateTime());
+    }
+}
