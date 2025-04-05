1c6abb92a4e5075ccefb822e4d7a772d83840c0e
Daryl <daryl@clubdeuce.net>
2025-04-04 23:03:39 -0400

    Adds Performances resource
    Adds the Performances resource for retrieving performance data from the Tessitura API.
    
    Includes methods for searching performances, retrieving upcoming performances, and retrieving performances within a date range.
    
    Also includes unit tests for the Performances resource.

---
+++ src/Resources/Performances.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Clubdeuce\Tessitura\Resources;
+
+use Clubdeuce\Tessitura\Base\Base;
+use Clubdeuce\Tessitura\Helpers\Api;
+use DateTime;
+
+class Performances extends Base
+{
+
+    const RESOURCE = 'TXN/Performances';
+    protected Api $_api;
+
+    public function __construct(Api $api = null)
+    {
+        if (empty ($api)) {
+            $api = new Api();
+        }
+
+        $this->_api = $api;
+    }
+
+    public function get_upcoming_performances(int $days = 30): array
+    {
+        try {
+            $start = new DateTime();
+            $end = new DateTime("now + {$days} days");
+
+            return $this->get_performances_between($start, $end);
+        } catch (\Exception $e) {
+            trigger_error($e->getMessage(), E_USER_WARNING);
+        }
+
+        return [];
+    }
+
+    /**
+     * @param DateTime $start
+     * @param DateTime $end
+     * @param array $args
+     *
+     * @return Performance[]
+     */
+    public function get_performances_between(DateTime $start, DateTime $end, array $args = []): array
+    {
+        $sorted = [];
+        $performances = $this->search(array(
+            'PerformanceStartDate' => $start->format(DATE_ATOM),
+            'PerformanceEndDate' => $end->format(DATE_ATOM),
+        ));
+
+        // generate a hash table
+        foreach ($performances as $performance) {
+            $sorted[$performance->date()->getTimestamp()] = $performance;
+        }
+
+        ksort($sorted);
+
+        return $sorted;
+    }
+
+    /**
+     * @param array $args
+     *
+     * @return Performance[]
+     * @link   https://docs.tessitura.com/REST_v151/TessituraService/HELP/API/POST_TXN_PERFORMANCES_SEARCH.HTM
+     */
+    public function search(array $args = []): array
+    {
+        $endpoint = sprintf('%1$s/Search', self::RESOURCE);
+        $body = json_encode($args);
+
+        $args = array(
+            'body' => $body,
+            'headers' => [
+                'Content-Length' => strlen($body)
+            ]
+        );
+
+        $results = $this->_api->post($endpoint, $args);
+
+        if (!is_array($results)) {
+            return [];
+        }
+
+        return array_map(function ($item) {
+            return new Performance($item);
+        }, $results);
+    }
+
+    /**
+     * @param int $psid
+     *
+     * @return Performance[]
+     */
+    public function get_performances_for_production_season(int $ps_id): array
+    {
+        return $this->search([
+            'ProductionSeasonIds' => $ps_id,
+        ]);
+    }
+
+}
---
+++ tests/unit/PerformancesTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Clubdeuce\Tessitura\Tests;
+
+use Clubdeuce\Tessitura\Resources\Performances;
+use PHPUnit\Framework\Attributes\CoversClass;
+use Clubdeuce\Tessitura\Helpers\Api;
+
+#[CoversClass(Performances::class)]
+class PerformancesTest extends testCase
+{
+    public function testSearchReturnsEmptyArray()
+    {
+        $api = $this->createMock(Api::class);
+        $api->method('post')->willReturn([]);
+
+        $sut = new Performances($api);
+        $result = $sut->search();
+
+        $this->assertIsArray($result);
+        $this->assertEmpty($result);
+    }
+
+    public function testSearch()
+    {
+        $api = $this->createMock(Api::class);
+        $api->method('post')
+            ->willReturn(json_decode(file_get_contents(dirname(__DIR__) . '/fixtures/performances.json'), 'associative'));
+
+        $sut = new Performances($api);
+        $result = $sut->search();
+
+        $this->assertIsArray($result);
+        $this->assertNotEmpty($result);
+    }
+
+    public function testGetPerformancesBetween()
+    {
+        $api = $this->createMock(Api::class);
+        $api->method('post')
+            ->willReturn(json_decode(file_get_contents(dirname(__DIR__) . '/fixtures/performances.json'), 'associative'));
+
+        $sut = new Performances($api);
+
+        // the dates here are irrelevant, as we are using a mock api response
+        $result = $sut->get_performances_between(new \DateTime(), new \DateTime());
+
+        $this->assertIsArray($result);
+        $this->assertNotEmpty($result);
+
+        $previous = $result[array_key_first($result)];
+
+        foreach ($result as $index => $current) {
+            $this->assertEquals($index, $current->date()->getTimestamp(), 'The array index is not the performance timestamp.');
+            $this->assertGreaterThanOrEqual($previous->date()->getTimestamp(), $index, 'The performance array is not sorted correctly.');
+            $previous = $current;
+        }
+
+    }
+}
