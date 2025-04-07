e499b1f65e8c84bffd81c1d23c36f62732fb946f
Daryl <daryl@clubdeuce.net>
2025-04-07 14:55:49 -0400

    Refactor Base class to improve type safety and documentation
    Updated method return types for better clarity and type safety. Added detailed docblocks to improve code readability and maintainability, providing comprehensive descriptions for methods, parameters, and properties.

--- src/Base/Base.php
+++ src/Base/Base.php
@@ -3,17 +3,30 @@
 namespace Clubdeuce\Tessitura\Base;
 
 /**
- * @method array extra_args()
+ * @method mixed extra_args()
  */
 class Base {
 
+    /**
+     * @var string[]
+     */
     protected array $_extra_args = [];
 
+    /**
+     * Constructor method for initializing the object state.
+     *
+     * @param string[] $args Optional array of arguments to set the object's state.
+     * @return void
+     */
     public function __construct( array $args = [] )
     {
         $this->_set_state( $args );
     }
 
+    /**
+     * @param  string[] $args
+     * @return void
+     */
     protected function _set_state( array $args = [] ) : void
     {
         foreach ( $args as $key => $value ) {
@@ -30,7 +43,14 @@ class Base {
 
     }
 
-    public function __call( string $name, array $args = [] )
+    /**
+     * Magic method to handle dynamic calls to inaccessible or non-existing methods.
+     *
+     * @param  string $name The name of the method being called.
+     * @param  mixed[] $args Optional array of arguments passed to the method call.
+     * @return mixed The value of the corresponding property or extra argument if available, or false if neither exists.
+     */
+    public function __call( string $name, array $args = [] ): mixed
     {
         $property = "_{$name}";
 
@@ -45,6 +65,13 @@ class Base {
         return false;
     }
 
+    /**
+     * Merges provided arguments with default values.
+     *
+     * @param  mixed[] $args Array of arguments to override the defaults.
+     * @param  mixed[] $defaults Array of default key-value pairs.
+     * @return mixed[] The merged array where defaults are overridden by provided arguments.
+     */
     public function parse_args( array $args = [], array $defaults = [] ) : array
     {
         foreach ( $defaults as $key => $value ) {
