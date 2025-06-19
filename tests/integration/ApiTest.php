dfcb77ca3f82eef2fb9872b1037c133a66a02544
Daryl Lozupone <daryl@clubdeuce.net>
2025-06-18 23:30:51 -0400

    Update `ApiTest` to expect `ConnectException` instead of `Error` in test case

--- tests/integration/ApiTest.php
+++ tests/integration/ApiTest.php
@@ -5,6 +5,7 @@ namespace Clubdeuce\Tessitura\Tests\Integration;
 use Clubdeuce\Tessitura\Helpers\Api;
 use Clubdeuce\Tessitura\Tests\TestCase;
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ConnectException;
 use GuzzleHttp\Psr7\Response;
 use GuzzleHttp\Psr7\Stream;
 use PHPUnit\Framework\Attributes\CoversClass;
@@ -31,17 +32,20 @@ class ApiTest extends TestCase
         $this->assertNotEmpty($response);
     }
 
+    /**
+     * @throws Exception
+     */
     public function testGet404Error(): void
     {
-        $stream = new Stream(fopen(dirname(__DIR__) . '/fixtures/season.json', 'r'));
+        $stream   = new Stream(fopen(dirname(__DIR__) . '/fixtures/season.json', 'r'));
         $response = new Response(404, [], $stream);
-        $client = $this->createMock(Client::class);
+        $client   = $this->createMock(Client::class);
 
         $client->method('get')->willReturn($response);
 
         $api = new Api(['baseRoute' => 'https://api.tessitura.com/', 'client' => $client]);
 
-        $this->expectException(\Error::class);
+        $this->expectException(ConnectException::class);
         $api->get('performances', []);
     }
 }
