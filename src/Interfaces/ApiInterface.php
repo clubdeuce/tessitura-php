e5863a1b069b860b32229d2ee6526693298f474a
Daryl Lozupone <daryl@clubdeuce.net>
2025-06-18 21:43:44 -0400

    Extend `ApiInterface` with additional interfaces and rename `base_route` to `baseRoute`

--- src/Interfaces/ApiInterface.php
+++ src/Interfaces/ApiInterface.php
@@ -2,35 +2,14 @@
 
 namespace Clubdeuce\Tessitura\Interfaces;
 
-use Exception;
-use GuzzleHttp\Client;
 use Psr\Log\LoggerInterface;
 
-/**
- * Interface ApiInterface
- * @package Clubdeuce\Tessitura\Interfaces
- */
-interface ApiInterface
+interface ApiInterface extends RequestHandlerInterface, ConfigurableApiInterface, ClientConfigurableInterface
 {
-    /**
-     * @param string $resource
-     * @param array $args
-     * @return mixed
-     * @throws Exception
-     */
-    public function get(string $resource, array $args = []): mixed;
-
-    /**
-     * @param string $endpoint
-     * @param array $args
-     * @return array|Exception
-     */
-    public function post(string $endpoint, array $args = []): array|Exception;
-
     /**
      * @return string
      */
-    public function base_route(): string;
+    public function baseRoute(): string;
 
     /**
      * @return string
