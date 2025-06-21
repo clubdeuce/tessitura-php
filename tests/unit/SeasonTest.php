bf7b75502e3b15fbe8bda704f1a52873d99e3e68
Daryl Lozupone <daryl@clubdeuce.net>
2025-06-20 21:01:58 -0400

    Refactor SeasonTest: update property access from underscore to standard syntax

--- tests/unit/SeasonTest.php
+++ tests/unit/SeasonTest.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Clubdeuce\Tessitura\Tests\Unit;
+
 use Clubdeuce\Tessitura\Resources\Season;
 use Clubdeuce\Tessitura\Tests\testCase;
 use PHPUnit\Framework\Attributes\CoversClass;
@@ -10,29 +12,29 @@ class SeasonTest extends testCase
     /**
      * @var Season
      */
-    protected Season $_sut;
+    protected Season $sut;
 
     public function setUp(): void
     {
         $data = json_decode(file_get_contents(dirname(__DIR__) . '/fixtures/season.json'), 'associative');
-        $this->_sut = new Season($data);
+        $this->sut = new Season($data);
     }
 
     public function testId(): void
     {
-        $this->assertIsInt($this->_sut->getId());
-        $this->assertEquals(194, $this->_sut->getId());
+        $this->assertIsInt($this->sut->getId());
+        $this->assertEquals(194, $this->sut->getId());
     }
 
     public function testDescription()
     {
-        $this->assertIsString($this->_sut->getDescription());
-        $this->assertEquals('2024/2025 Opera', $this->_sut->getDescription());
+        $this->assertIsString($this->sut->getDescription());
+        $this->assertEquals('2024/2025 Opera', $this->sut->getDescription());
     }
 
     public function testCreatedDateTime(): void
     {
-        $created = $this->_sut->getCreatedDateTime();
+        $created = $this->sut->getCreatedDateTime();
 
         $this->assertInstanceOf(\DateTime::class, $created);
         $this->assertEquals('2024-01-19', $created->format('Y-m-d'));
@@ -40,7 +42,7 @@ class SeasonTest extends testCase
 
     public function testStartDateTime()
     {
-        $start = $this->_sut->getStartDateTime();
+        $start = $this->sut->getStartDateTime();
 
         $this->assertInstanceOf(\DateTime::class, $start);
         $this->assertEquals('2024-07-01', $start->format('Y-m-d'));
@@ -48,7 +50,7 @@ class SeasonTest extends testCase
 
     public function testEndDateTime()
     {
-        $end = $this->_sut->getEndDateTime();
+        $end = $this->sut->getEndDateTime();
 
         $this->assertInstanceOf(\DateTime::class, $end);
         $this->assertEquals('2025-06-30', $end->format('Y-m-d'));
