3bd1e6b324a806f5b223ed2daaa261f0da60b440
Daryl Lozupone <daryl@clubdeuce.net>
2025-06-20 23:33:47 -0400

    Add RoboFile for task automation with PHPStan, PHP CodeSniffer, and PHPUnit
    skip-ci

---
+++ RoboFile.php
@@ -0,0 +1,48 @@
+<?php
+
+use Robo\Result;
+
+/**
+ * This is project's console commands configuration for Robo task runner.
+ *
+ * @see https://robo.li/
+ */
+class RoboFile extends \Robo\Tasks
+{
+    // define public methods as commands
+    /**
+     * Run PHPStan static analysis.
+     */
+    public function phpstan(): Result
+    {
+        return $this->taskExec('vendor/bin/phpstan analyse src --memory-limit=1G')->run();
+    }
+
+    /**
+     * Run PHP CodeSniffer.
+     */
+    public function phpcs(): Result
+    {
+        return $this->taskExec('vendor/bin/phpcs src')->run();
+    }
+
+    /**
+     * Run PHPUnit tests.
+     */
+    public function phpunit(): Result
+    {
+        return $this->taskPHPUnit()
+            ->configFile('tests/phpunit.xml.dist')
+            ->run();
+    }
+
+    /**
+     * Run all quality checks.
+     */
+    public function quality(): void
+    {
+        $this->phpstan();
+        $this->phpcs();
+        $this->phpunit();
+    }
+}
